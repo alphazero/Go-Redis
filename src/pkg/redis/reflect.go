485698c3ea5581bb20bc471f4a6c1054edf82e70
Joubin Houshyar <alphazero@sensesay.net>
2009-11-16 22:32:27 -0500

    Added a new file for sync utils.  Renamed reflect_utils.go to reflect.

---
+++ src/pkg/redis/reflect.go
@@ -0,0 +1,59 @@
+//   Copyright 2009 Joubin Houshyar
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//    
+//   http://www.apache.org/licenses/LICENSE-2.0
+//    
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+
+package redis
+
+import (
+	"reflect";
+)
+
+// ----------------------------------------------------------------------------
+//	reflect contains a few helper functions to facilitate working with
+//	the reflect artifacts of Go.  Probably deserves to be in its own
+//	(independent) little package as using Go reflection is quite tedious.
+// ----------------------------------------------------------------------------
+
+// TODO: document me
+//
+func GetByteArrayAtIndex(v *reflect.StructValue, i int) (arr []byte, ok bool){
+	field := v.Field(i);
+	return GetByteArray (field);
+}
+
+// TODO: document me
+//
+func GetByteArray (v reflect.Value) (arr []byte, ok bool) {
+	switch v := v.(type) {
+	case reflect.ArrayOrSliceValue:
+		aosv := reflect.ArrayOrSliceValue(v);
+		arr = make([]byte, aosv.Len());
+		for i:=0; i<aosv.Len();i++ {
+			arr[i] = aosv.Elem(i).(*reflect.Uint8Value).Get(); 
+		} 
+		return arr, true;
+	}
+	return;
+}
+
+// TODO: document me
+//
+func GetByteArrayLen (v reflect.Value) (len int, ok bool) {
+	switch v := v.(type) {
+	case reflect.ArrayOrSliceValue:
+		aosv := reflect.ArrayOrSliceValue(v);
+		return aosv.Len(), true;
+	}
+	return;
+}
---
+++ src/pkg/redis/sync.go
@@ -0,0 +1,53 @@
+//   Copyright 2009 Joubin Houshyar
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//    
+//   http://www.apache.org/licenses/LICENSE-2.0
+//    
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//
+package redis
+
+import (
+	"time";
+)
+
+// ----------------------------------------------------------------------------
+// synchronization utilities.
+// ----------------------------------------------------------------------------
+
+// Timer
+//
+// start a new time that will signal on the returned
+// channel when the specified ns (timeout in nanoseconds)
+// have passsed.  If ns < 0, function returns immediately
+// with nil.  Otherwise, the call can select on the channel
+// and will recieve an item after timeout.  If the timer
+// itself was interrupted during sleep, the value in channel
+// will be 0-time-elapsed.  Otherwise, for normal operation,
+// it will return time elapsed in ns (which hopefully is very
+// close to the specified ns.
+//
+
+func NewTimer (ns int64) (signal <-chan int64) {
+    if ns <= 0 {
+        return nil
+    }
+    c := make(chan int64);
+    go func() {
+    	t := time.Nanoseconds();
+    	e := time.Sleep(ns);
+    	if e != nil { 
+    		t = 0 - (time.Nanoseconds() - t);
+    	}
+    	t = time.Nanoseconds() - t;
+    	c<- t;
+    }();
+    return c;
+}

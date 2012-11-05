eb96590f4f1cc7f6c22d3bc947da6969b3303d0a
Iurii Ogiienko <ogiyenko@gmail.com>
2012-11-05 18:16:43 +0200

    added ZREMRANGEBYSCORE declaration to AsyncClient interface

--- redis.go
+++ redis.go
@@ -502,6 +502,9 @@ type AsyncClient interface {
 	// Redis ZRANGEBYSCORE command.
 	Zrangebyscore(key string, arg1 float64, arg2 float64) (result FutureBytesArray, err Error)
 
+	// Redis ZREMRANGEBYSCORE command
+	Zremrangebyscore(key string, arg1 float64, arg2 float64) (result FutureInt64, err Error)
+
 	// Redis FLUSHDB command.
 	Flushdb() (status FutureBool, err Error)
 
